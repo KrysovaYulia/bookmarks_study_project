7edc920b9d29b7731893a148974ebffc671442c5
yuliakrysova <kryuma2oos@gmail.com>
2025-06-19 00:28:41 +0300

    Add user registration

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -105,3 +105,12 @@ LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+MEDIA_URl = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+'account.authentication.EmailAuthBackend',
+]
